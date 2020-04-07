075844a5269b5fdd3596b2c64487f6a33500f96c
fawwazmayda <maydafawwaz@gmail.com>
2020-04-07 16:45:58 +0800

    Initial Commit

---
+++ Alarmy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Alarmy
+//
+//  Created by Muhammad Fawwaz Mayda on 07/04/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
