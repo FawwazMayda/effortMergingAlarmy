98f2d1ae029db82b2575c364a6561517bbcfbb7f
fawwazmayda <maydafawwaz@gmail.com>
2020-04-07 17:20:47 +0800

    making individual VC

---
+++ Alarmy/ViewController/Alarm/FirstAlarmVC.swift
@@ -0,0 +1,30 @@
+//
+//  FirstAlarmVC.swift
+//  Alarmy
+//
+//  Created by Muhammad Fawwaz Mayda on 07/04/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class FirstAlarmVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
